d2c88860f8f427d57a5a95376cd552cd5b5d907c
Musigwa <pacifique.musigwa@gmail.com>
2020-04-16 12:43:54 +0200

    rename the application

--- ios/MovementClearanceTests/MovementClearanceTests.m
+++ ios/MovementClearanceTests/MovementClearanceTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface UruhushyaTests : XCTestCase
+@interface MovementClearanceTests : XCTestCase
 
 @end
 
-@implementation UruhushyaTests
+@implementation MovementClearanceTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,8 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { persistStore, persistReducer } from 'redux-persist';
-// import storage from 'redux-persist/lib/storage';
 import AsyncStorage from '@react-native-community/async-storage';
 import hardSet from 'redux-persist/lib/stateReconciler/hardSet';
-// import AsyncStorage from '@react-native-community/async-storage';
 import reducers from './reducers';
 
 const persistConfig = {
